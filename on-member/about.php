8bfa61f59389f3418720e8debcd6e598bd62b64f
bayuswara <54299369+bayuswara@users.noreply.github.com>
2020-04-23 18:11:46 +0700

    Add files via upload

---
+++ on-member/about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="bootstrap/css/header.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Checkup Yourself</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+<div class = "row"> 
+<div class = "col-md-6">
+          <p> Perkenalkan Saya Bayu Swara Mahardika
+              Saya Belajar Di SMK Telkom Malang
+              Saya Dari Kelas XR2, Saya Berasal dari Kota
+              Malang 
+               
+               
+        </div>
+<div class = "col-md-6">
+            <img src="pinkpanther.jpg" width="700" height="450" >
+</div>
+</div>
+       
+       
+  <script src="bootstrap/js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ on-member/index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Corona Check</title>
+    <link rel="stylesheet" href="reza.css" />
+    <!-- untuk menghubungkan ke file css -->
+    <link rel="icon" href="fav.png" />
+    <!-- untuk menampilkan icon -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" />
+  </head>
+  <body>
+    <!-- ini untuk menu -->
+    <div class="menu">
+      
+      <ul>
+        <li><a href="index.php">Beranda</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="Check.php">Check</a></li>
+        <li><a href="#">Logout</a></li>
+      
+      </ul>
+    </div>
+
+    <!-- ini untuk header -->
+    <div class="header">
+      <div class="bayangan">
+      </div>
+      <h1>
+        
+        <br>
+         Website About Corona
+      </h1>
+    </div>
+
+    <div class="about">
+      <div class="left">
+        <h1>
+          Tentang
+          <br>
+          WEB
+          <br>
+          CORONA
+        </h1>
+        <p>
+         Virus Corona adalah sebuah keluarga virus yang ditemukan
+         pada manusia dan hewan
+          
+        </p>
+      </div>
+      <div class="right">
+        <img src="corons.jpg" />
+      </div>
+    </div>
+
+    <div class="layanan">
+      <div class="box">
+        <div class="bunder bg-navy">
+          <span class="fab fa-facebook"></span>
+        </div>
+        <h3 class="text-navy">Info On FaceBook</h3>
+        <div class="deskripsi">
+          Berhati - hatilah Hoax Yang Menyebar
+          Di Socmed terutama FaceBook
+        </div>
+      </div>
+      <div class="box">
+        <div class="bunder bg-grey">
+          <span class="fa fa-home"></span>
+        </div>
+        <h3 class="text-grey">Stay At Home</h3>
+        <div class="deskripsi">
+        Salah satu Pencegahan penularan
+        Virus Corona adalah melakukan Stay At Home 
+        Karena Berada di rumah saja akan menurunkan 
+        resiko tertular
+        </div>
+      </div>
+      <div class="box">
+        <div class="bunder bg-green">
+          <span class="fa fa-users"></span>
+        </div>
+        <h3 class="text-green">Social Distanding</h3>
+        <div class="deskripsi">
+          Salah satu Pencegahan penularan
+          Virus Corona adalah melakukan social distanding
+        </div>
+      </div>
+      <div class="box">
+        <div class="bunder bg-blue">
+          <span class="fa fa-building"></span>
+        </div>
+        <h3 class="text-blue">Jangan Berpergian Jauh</h3>
+        <div class="deskripsi">
+          Karena Berpergian Jauh akan Menaikan 
+          Resiko Tertular atau menularkan Virus
+          Corona
+        </div>
+      </div>
+    </div>
+
+    <div class="penghargaan">
+      <div class="gelap">
+      </div>
+      <div class="left">
+        <h2>Corona </h2>
+        <h1>Website <span>Cool</span></h1>
+        <h3></h3>
+      </div>
+     
+
+    </div>
+
+    <div class="studi">
+      <div class="bunder">
+        <span class="fa fa-graduation-cap"></span>
+      </div>
+      <h1>Belajar <span class="text-purple">Di Rumah</span></h1>
+      <p>
+        Memang Karena Pandemi Virus Corona Sekolah - Sekolah 
+        Banyak yang Menutup Sekolah tetapi meskipun Sekolah ditutup kita 
+        Tidak Boleh Bermain main Saja Kita Harus Memanfaatkan Waktu Kita 
+        Dengan Belajar Di rumah
+      </p>
+    </div>
+
+    <div class="prodi">
+      <img src="prodi.png" alt="">
+      <div class="jurusan">
+        <h3>Rekayasa Perangkat Lunak</h3>
+        <p>
+          Mempelajari seluruh aspek produksi software
+          seperti aplikasi website, aplikasi android,
+           maupun aplikasi dekstop.
+        </p>
+      </div>
+      <div class="jurusan">
+        <h3>Teknik Komputer dan Jaringan</h3>
+        <p>
+          Mempelajari cara merakit dan instalasi komputer,
+          instalasi jaringan Local Area Network (LAN) dan
+           Wide Area Network (WAN).
+        </p>
+      </div>
+    </div>
+
+    <div class="prestasi">
+      <div class="left">
+        <h1>WOW </h1>
+        <p>
+        
+        </p>
+      </div>
+      <div class="right">
+        <img src="prestasi.jpg" alt="">
+      </div>
+      <div class="footer">
+        
+      </div>
+    </div>
+
+    <div class="info">
+      <div class="bunder">
+        <span class="fa fa-newspaper"></span>
+      </div>
+      <h1>
+        Info <span class="text-purple">Terbaru</span>
+      </h1>
+      <p>
+        Ikuti terus informasi dan kegiatan terbaru
+        tentang Website Kita
+      </p>
+    </div>
+
+    <div class="update">
+      <div class="centang">
+        <span class="fa fa-check"></span>
+      </div>
+      <div class="left">
+        <h1>Tetap Up to Date</h1>
+        <p>
+          Anda juga bisa mengikuti kita di media sosial
+        </p>
+      </div>
+      <div class="right">
+        <div class="bunder">
+          <span class="fab fa-facebook"></span>
+        </div>
+        <div class="bunder">
+          <span class="fab fa-instagram"></span>
+        </div>
+        <div class="bunder">
+          <span class="fab fa-twitter"></span>
+        </div>
+        <div class="bunder">
+          <span class="fab fa-facebook"></span>
+        </div>
+      </div>
+    </div>
+    
+     
+      </div>
+    </div>
+    <div class="foo">
+      <div class="col">
+        
+        <p>
+          
+        </p>
+        <h3>Media Sosial</h3>
+        <span class="fab fa-facebook"></span>
+        <span class="fab fa-instagram"></span>
+        <span class="fab fa-twitter"></span>
+        <span class="fab fa-google"></span>
+      </div>
+      <div class="col"> <!-- bagian kedua -->
+        <h3>Berita Terbaru</h3>
+      </div>
+      <div class="col"> <!-- bagian ketiga -->
+        <h3>Lokasi Kami</h3>
+        <h4><span class="fa fa-map-marker"> </span> </h4>
+        <h4><span class="fa fa-phone"></span> </h4>
+        <h4><span class="fa fa-envelope"> </span> </h4>
+        <h4><span class="fa fa-clock"></span> </h4>
+      </div>
+      <div class="col">
+
+      </div>
+    </div>
+    <div class="footers">
+      <h5>Copyright 2020 © SMK Telkom Malang. All rights reserved.</h5>
+    </div>
+    <!-- <div class="foo">
+      <div class="col">
+        <img src="moklet.png" alt="">
+        <p>
+          Pelopor SMK bidang Teknologi dan Informatika
+          di Indonesia. Berpengalaman dari tahun 1992 yang
+          telah terakreditasi "A" dan mempunyai standart mutu ISO 9001:2015.
+        </p>
+        <h3>Media Sosial</h3>
+        <span class="fab fa-facebook"></span>
+        <span class="fab fa-instagram"></span>
+        <span class="fab fa-twitter"></span>
+        <span class="fab fa-google"></span>
+      </div>
+      <div class="col">
+
+      </div>
+      <div class="col">
+
+      </div>
+      <div class="col">
+
+      </div>
+    </div> -->
+  </body>
+</html>
